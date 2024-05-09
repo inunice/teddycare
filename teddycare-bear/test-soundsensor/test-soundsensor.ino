14248796d5f4cad3c4fd0be90a6005feeab7cc4a
inunice <ammanguan@up.edu.ph>
2024-05-09 12:10:53 +0800

    [bear] update test for sound sensor

--- teddycare-bear/test-soundsensor/test-soundsensor.ino
+++ teddycare-bear/test-soundsensor/test-soundsensor.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 #define SOUND_SENSOR_PIN 35
+#define SOUND_THRESHOLD 3500
 
 void setup() {
   Serial.begin(9600);
@@ -9,7 +10,11 @@ void setup() {
 
 void loop() {
   // Read the analog value from the sound sensor
-  int sensorValue = analogRead(SOUND_SENSOR_PIN);
-  Serial.println(sensorValue);
+  int soundValue = analogRead(SOUND_SENSOR_PIN);
+  Serial.println(soundValue);
+
+  if (soundValue > SOUND_THRESHOLD) {
+    Serial.println("baby uwu cry");
+  }
   delay(100);
 }
