5898ee1b5885bcaea09703822b7f79132277d36e
Calvin James Maximo <calvin.james.12391@gmail.com>
2024-05-19 19:57:09 +0800

    feat[bear]: add fetching of filename of audio currently playing

---
+++ teddycare-fetch/teddycare-fetch.ino
@@ -0,0 +1,88 @@
+// Libraries
+#include <Arduino.h>
+// Wifi
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+// // Time
+// #include "time.h"
+
+// Network credentials
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+// Firebase credentials
+#define API_KEY ""
+#define DATABASE_URL "https://teddycare-12aaf-default-rtdb.asia-southeast1.firebasedatabase.app" 
+
+// Firebase
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+// Firebase variables
+unsigned long sendDataPrevMillis = 0;
+bool signupOK = false;
+
+// Variables
+String stringValue = "";
+
+void setup() {
+  // put your setup code here, to run once:
+  // Initialize serial communication
+  Serial.begin(115200);
+
+  // Wi-Fi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+
+  // Printing for Wi-Fi connection
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  // Accessing Firebase
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else {
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  // Firebase connection
+  config.token_status_callback = tokenStatusCallback;
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  Serial.println("Setup complete!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 1000 || sendDataPrevMillis == 0)) {
+    sendDataPrevMillis = millis();
+    Serial.println("Ready to read from the database.");
+
+    if (Firebase.RTDB.getString(&fbdo, "/speaker/audioPlaying")) {
+      if (fbdo.dataType() == "string") {
+        stringValue = fbdo.stringData();
+        Serial.print("The current audio playing is: ");
+        Serial.print(stringValue);
+        Serial.println();
+      }
+    } else {
+      Serial.println("FAILED: " + fbdo.errorReason());
+    }
+  }
+
+  delay(1000);  // Delay between readings
+}
