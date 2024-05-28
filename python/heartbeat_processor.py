1b2ec8bf96c939fd297bb5e5b6eb74970183f320
AlJf-the-Coder <agjafri@up.edu.ph>
2024-05-28 22:19:49 +0800

    chore: move functions from beat_sample to heartbeat_processor and put heartbeat processing code in function preprocess()

--- python/heartbeat_processor.py
+++ python/heartbeat_processor.py
@@ -1,30 +1,51 @@
-from time import sleep
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-from beat_sample import get_bpm, get_peaks
+from scipy.signal import find_peaks
+from math import ceil
+
+def normalize(y, old_min, old_max, new_min = 150, new_max = 255):
+    a = (new_max - new_min) / (old_max - old_min)
+    b = new_max - a * old_max
+    return min(max(ceil(a * y + b), new_min), 255)
+
+def get_peaks(beat_pairs, normal = False):
+    timestamps = [pair['from_start_device_time'] for pair in beat_pairs]
+    values = [pair['ir_value'] for pair in beat_pairs]
+
+    # normalize values
+
+    # TODO: need to fix parameters 
+    peaks, _ = find_peaks(values, distance = 10)
+    # peaks = list(peaks)
+    if normal:
+        max_val = max(values[i] for i in peaks)
+        min_val = min(values[i] for i in peaks)
+        return [{'from_start_device_time': timestamps[i], 'ir_value': normalize(values[i], min_val, max_val)} for i in peaks]
+    else:
+        return [{'from_start_device_time': timestamps[i], 'ir_value': values[i]} for i in peaks]
+
+def get_bpm(beat_pairs):
+    num_peaks = len(get_peaks(beat_pairs))
+    return num_peaks * 2
+
+
 
 cred = credentials.Certificate('test1-a4e94-firebase-adminsdk-mv0uj-28a74beeb8.json')
 
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://test1-a4e94-default-rtdb.asia-southeast1.firebasedatabase.app/'
-})
+    })
 
-# As an admin, the app has access to read and write all data, regradless of Security Rules
-ref = db.reference('heartbeat_data')
-while True:
+
+# call this function whenever the is_recording value changes
+def preprocess():
+    # As an admin, the app has access to read and write all data, regradless of Security Rules
+    ref = db.reference('heartbeat_data')
     rec_ref = ref.child('recordings')
     is_recording = ref.child('is_recording').get()
     recordings = rec_ref.get()
-    
-    #test
-    '''
-    recording_list = []
-    for recording in recordings.values():
-        recording_list.extend(recording[:-1])
-    break
-    '''
 
     if is_recording == '0' and recordings != None and len(recordings) > 0:
         recording_list = []
@@ -38,30 +59,5 @@ while True:
                 }
             })
         # remove recordings
-        # ref.update({'recordings': None})
+        ref.update({'recordings': None})
         print('processed')
-    sleep(60)
-
-# testing
-'''
-import matplotlib.pyplot as plt
-beats = recording_list
-beats_x = [pair['from_start_device_time'] for pair in beats]
-beats_y = [pair['ir_value'] for pair in beats]
-print(beats_x)
-print(beats_y)
-
-peaks = get_peaks(beats)
-peaks_x = [pair['from_start_device_time'] for pair in peaks]
-peaks_y = [pair['ir_value'] for pair in peaks]
-print(peaks_x)
-print(peaks_y)
-
-plt.plot(beats_x, beats_y)
-plt.scatter(peaks_x, peaks_y)
-plt.title('Heartbeat Data')
-plt.xlabel('Time')
-plt.ylabel('Value')
-plt.show()
-'''
-
