64eb48b42473b5e9c3cb7ad77cfc83f1e7792b26
AlJf-the-Coder <agjafri@up.edu.ph>
2024-05-29 00:12:23 +0800

    feat: now calculate troughs (buggy) and update format of data pushed to database

--- python/heartbeat_processor.py
+++ python/heartbeat_processor.py
@@ -4,40 +4,50 @@ from firebase_admin import db
 from scipy.signal import find_peaks
 from math import ceil
 
-def normalize(y, old_min, old_max, new_min = 150, new_max = 255):
+def normalize(y, old_min, old_max, new_min = 100, new_max = 200):
     a = (new_max - new_min) / (old_max - old_min)
     b = new_max - a * old_max
     return min(max(ceil(a * y + b), new_min), 255)
 
-def get_peaks(beat_pairs, normal = False):
+def get_troughs(beat_pairs):
+    timestamps = [pair['from_start_device_time'] for pair in beat_pairs]
+    values = [pair['ir_value'] for pair in beat_pairs]
+    neg_values = [-value for value in values]
+
+    # TODO: need to fix parameters 
+    troughs, _ = find_peaks(neg_values, distance = 5)
+    return [{'from_start_device_time': timestamps[i], 'ir_value': values[i], 'peak': 0} for i in troughs]
+
+def get_peaks(beat_pairs):
     timestamps = [pair['from_start_device_time'] for pair in beat_pairs]
     values = [pair['ir_value'] for pair in beat_pairs]
 
-    # normalize values
-
     # TODO: need to fix parameters 
-    peaks, _ = find_peaks(values, distance = 10)
-    # peaks = list(peaks)
-    if normal:
-        max_val = max(values[i] for i in peaks)
-        min_val = min(values[i] for i in peaks)
-        return [{'from_start_device_time': timestamps[i], 'ir_value': normalize(values[i], min_val, max_val)} for i in peaks]
-    else:
-        return [{'from_start_device_time': timestamps[i], 'ir_value': values[i]} for i in peaks]
+    peaks, _ = find_peaks(values, distance = 5)
+    return [{'from_start_device_time': timestamps[i], 'ir_value': values[i], 'peak': 1} for i in peaks]
 
-def get_bpm(beat_pairs):
-    num_peaks = len(get_peaks(beat_pairs))
-    return num_peaks * 2
+def calculate_delays(beat_pairs):
+    peaks = get_peaks(beat_pairs)
+    troughs = get_troughs(beat_pairs)
+    spikes = peaks + troughs
+    spikes.sort(key = lambda tup: tup['from_start_device_time'])
 
+    #calculate delays
+    for i in range(1, len(spikes)):
+        spikes[i]['delay'] = spikes[i]['from_start_device_time'] - spikes[i - 1]['from_start_device_time']
+    spikes[0]['delay'] = spikes[-1]['delay']
 
+    # remove extra attribute
+    for spike in spikes:
+        del spike['from_start_device_time']
 
-cred = credentials.Certificate('test1-a4e94-firebase-adminsdk-mv0uj-28a74beeb8.json')
+    # convert values to pwm
+    max_val = max(spike['ir_value'] for spike in spikes)
+    min_val = min(spike['ir_value'] for spike in spikes)
+    for spike in spikes:
+        spike['ir_value'] = normalize(spike['ir_value'], min_val, max_val)
 
-# Initialize the app with a service account, granting admin privileges
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://test1-a4e94-default-rtdb.asia-southeast1.firebasedatabase.app/'
-    })
-
+    return spikes
 
 # call this function whenever the is_recording value changes
 def preprocess():
@@ -54,10 +64,54 @@ def preprocess():
         # preprocess
         ref.update({
             'preprocessed_heartbeat': {
-                'frequency': get_bpm(recording_list),
-                'peaks': get_peaks(recording_list, normal = True),
+                'spikes': calculate_delays(recording_list)
                 }
             })
         # remove recordings
-        ref.update({'recordings': None})
+        # ref.update({'recordings': None})
         print('processed')
+
+cred = credentials.Certificate('test1-a4e94-firebase-adminsdk-mv0uj-28a74beeb8.json')
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://test1-a4e94-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    })
+
+# test
+def test():
+    import matplotlib.pyplot as plt
+
+    ref = db.reference('heartbeat_data')
+    rec_ref = ref.child('recordings')
+    recordings = rec_ref.get()
+    recording_list = []
+    for recording in recordings.values():
+        recording_list.extend(recording[:-1])
+
+    beats = recording_list
+
+    # delays = [beats[i]['from_start_device_time'] - beats[i - 1]['from_start_device_time'] for i in range(1, len(beats))]
+    # print(f'average delay is {sum(delays) / len(delays)}')
+
+    beats_x = [pair['from_start_device_time'] for pair in beats]
+    beats_y = [pair['ir_value'] for pair in beats]
+
+    peaks = get_peaks(beats)
+    peaks_x = [pair['from_start_device_time'] for pair in peaks]
+    peaks_y = [pair['ir_value'] for pair in peaks]
+
+    troughs = get_troughs(beats)
+    troughs_x = [pair['from_start_device_time'] for pair in troughs]
+    troughs_y = [pair['ir_value'] for pair in troughs]
+
+    plt.plot(beats_x, beats_y)
+    plt.scatter(peaks_x, peaks_y, c = 'green')
+    plt.scatter(troughs_x, troughs_y, c = 'red')
+    plt.title('Heartbeat Data')
+    plt.xlabel('Time')
+    plt.ylabel('Value')
+    plt.show()
+
+    preprocess()
+
